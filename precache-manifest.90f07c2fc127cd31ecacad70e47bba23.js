cf6cdc18d94c4ffb7d8f8ed2c3a66ef6d1ea3c0a
somaratne <eranga_c@yahoo.ca>
2019-09-10 23:04:55 +0530

    Updates

---
+++ precache-manifest.90f07c2fc127cd31ecacad70e47bba23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e55681abb6ad81114f1ca497a81d136",
+    "url": "/monsters-rolodex-ii/index.html"
+  },
+  {
+    "revision": "99fdd6a93d67bcc77f6d",
+    "url": "/monsters-rolodex-ii/static/css/main.7d926a01.chunk.css"
+  },
+  {
+    "revision": "7877c2024a75e3da7c4e",
+    "url": "/monsters-rolodex-ii/static/js/2.a07f8c74.chunk.js"
+  },
+  {
+    "revision": "99fdd6a93d67bcc77f6d",
+    "url": "/monsters-rolodex-ii/static/js/main.9b73a30c.chunk.js"
+  },
+  {
+    "revision": "eefa893c7b970dcc866b",
+    "url": "/monsters-rolodex-ii/static/js/runtime~main.90f6e81f.js"
+  }
+]);
